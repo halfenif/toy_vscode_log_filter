a1ff5e9084181ceaf94a9bb0b184a33714a015a5
halfenif <halfenif@gmail.com>
2024-07-21 17:32:41 +0900

    need package

--- src/extension.ts
+++ src/extension.ts
@@ -6,23 +6,36 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	let disposable = vscode.commands.registerCommand('lineFilter.filterLines', async () => {
-		vscode.window.showInformationMessage("Called");
-
+	let disposable = vscode.commands.registerCommand('toy-vscode-log-filter.logfilter', async () => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			vscode.window.showInformationMessage("Not Editor");
 		  	return;
 		}
 	
+
+		const config = vscode.workspace.getConfiguration('logfilter');
+		const filterCharacter = config.get<string>('filterCharacter', '*');
+		const position = config.get<number>('position', 20);
+		const compareByte = filterCharacter.length;
+
 		const document = editor.document;
 		const newLines: string[] = [];
 	
 		for (let i = 0; i < document.lineCount; i++) {
-		  const line = document.lineAt(i);
-		  if (line.text.length >= 20 && line.text[19] === '*') {
-			newLines.push(line.text);
-		  }
+			const line = document.lineAt(i);
+
+			if (line.text.length < position + 1 + compareByte) {
+				continue;
+			}
+
+			let sourceText = line.text.substring(position + 1, position + 1 + compareByte);
+
+			// vscode.window.showInformationMessage(`line.text[${position}]:${sourceText}`);
+			
+			if (sourceText === filterCharacter) {
+				newLines.push(line.text);
+			}
 		}
 	
 		const newDocument = await vscode.workspace.openTextDocument({
